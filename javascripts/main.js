d06ded89d729a3430678ff8abb566844482c6092
Tony Trinh <tony19@gmail.com>
2012-12-29 21:32:10 -0600

    Add release notes and remove "Named" prefix

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,12 @@
 
 $(function() {
 	prettyPrint();
+
+	// release notes
+	$('.detail').hide();
+	$('.summary :not(:first)').hide();
+
+	$('#release-notes h3').click(function() {
+		$(this).find('~ .summary').slideToggle();
+	});
 });
