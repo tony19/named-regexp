e63bde4cfad268362a03bbcd95f36caa5e6f1eb0
Tony Trinh <tony19@gmail.com>
2017-06-23 00:28:37 -0500

    Workaround GPG keysign ioctl error

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Deploy release builds to Sonatype (and not the staging repo)
-mvn $1 -Prelease -Prelease-sign-artifacts release:clean release:prepare release:perform
+GPG_TTY=$(tty) mvn $1 -Prelease -Prelease-sign-artifacts release:clean release:prepare release:perform
 
 
 # To redeploy without touching git (tags, commits, etc), run:
